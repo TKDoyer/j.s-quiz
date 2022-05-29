3e75d8b02bf7230f41e2321c93f9d34087d56d83
Robert Doyer <tkdoyer@gmail.com>
2022-05-28 21:53:04 -0500

    quiz answer1 one

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+// make qustions
+const answerA = document.getElementById(`answerA`) 
+console.log(answerA)
+
+//answerA.addEventListener(`click` , function(e) {
+//console.log(`you clicked me!?!?`);
+//});
+
+
+
+
+//let answerA = document.querySelector(`#answerA`);
+let answerB = document.querySelector(`#answerB`);
+let answerC = document.querySelector(`#answerC`);
+let answerD = document.querySelector(`#answerD`);
+
